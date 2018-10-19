f0fabc95c8cc0e0fd16ef86239f500acb031e8d0
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-10-19 13:49:39 -0300

    :rocket: Nuget Released

---
+++ ConvertersPackSample/ConvertersPackSample/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ConvertersPackSample.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Property
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+
+
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        private decimal _money;
+        public decimal Money
+        {
+            get => _money;
+            set => SetProperty(ref _money, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        public MainViewModel()
+        {
+            Name = string.Empty;
+        }
+    }
+}
